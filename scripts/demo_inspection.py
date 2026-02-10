e7cad40608004d7b64503f0dc53b95d60805cb47
Oliver Coleman <colemanoliver6@gmail.com>
2026-02-10 17:40:51 -0400

    Updated demo_inspection.py

--- scripts/demo_inspection.py
+++ scripts/demo_inspection.py
@@ -51,6 +51,16 @@ def main():
         [0.0, 0.0, 0.0]
     ]
 
+
+    initial_pose = PoseStamped()
+    initial_pose.header.frame_id = 'map'
+    initial_pose.header.stamp = navigator.get_clock().now().to_msg()
+    initial_pose.pose.position.x = 0.3
+    initial_pose.pose.position.y = 0.3
+    initial_pose.pose.orientation.z = 0.0
+    initial_pose.pose.orientation.w = 1.0
+    navigator.setInitialPose(initial_pose)
+
     # Wait for navigation to fully activate
     navigator.waitUntilNav2Active()
 
